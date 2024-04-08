a10f5c3ab97c0d5f6670cf031cdfb57ef6cbf272
Andriy Borkovich <borkovich25andri@gmail.com>
2024-04-08 23:36:17 +0300

    added seats generation on bus creation
    used SQL SP and trigger for this, also added bus details view to EF core model snapshot

---
+++ src/BSMS.Infrastructure/Persistence/Migrations/20240408202716_AddSeatsGenerationOnBusCreation.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BSMS.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeatsGenerationOnBusCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE GenerateSeatsForBus
+                    @BusId INT,
+                    @SeatCount INT
+                AS
+                BEGIN
+                    DECLARE @SeatNumber INT = 1;
+
+                    WHILE @SeatNumber <= @SeatCount
+                    BEGIN
+                        INSERT INTO Seats (BusId, SeatNumber, IsFree)
+                        VALUES (@BusId, @SeatNumber, 1); -- Assuming all seats are initially free
+
+                        SET @SeatNumber = @SeatNumber + 1;
+                    END;
+                END;
+                GO
+
+                CREATE TRIGGER GenerateSeatsOnBusInsert
+                ON Buses
+                AFTER INSERT
+                AS
+                BEGIN
+                    DECLARE @BusId INT;
+
+                    SELECT @BusId = inserted.BusId FROM inserted;
+
+                    DECLARE @SeatCount INT;
+                    SELECT @SeatCount = Capacity FROM inserted;
+
+                    EXEC GenerateSeatsForBus @BusId, @SeatCount;
+                END;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP PROCEDURE IF EXISTS GenerateSeatsForBus;
+                DROP TRIGGER IF EXISTS GenerateSeatsOnBusInsert ON Buses;");
+        }
+    }
+}
